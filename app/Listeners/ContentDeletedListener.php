7f9bd3b428f84bb5cf98d165b5123e280873eb53
worfect <senchenkov.93@gmail.com>
2021-09-02 21:19:19 +0800

    add double cascade del comments attitudes

--- app/Listeners/ContentDeletedListener.php
+++ app/Listeners/ContentDeletedListener.php
@@ -3,6 +3,8 @@
 namespace App\Listeners;
 
 use App\Events\ContentDeleted;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class ContentDeletedListener
 {
@@ -15,6 +17,11 @@ class ContentDeletedListener
     public function handle(ContentDeleted $event)
     {
         if (method_exists($event->model, 'comments')){
+            $id = $event->model->comments()->get()->modelKeys();
+            DB::table('attitudes')->where('attitudeable_type', 'App\Models\Comment')
+                                        ->whereIn('attitudeable_id', $id)
+                                        ->update(['deleted_at' => Carbon::now()]);
+
             $event->model->comments()->delete();
         }
         if (method_exists($event->model, 'attitude')){
--- app/Listeners/ContentRestoredListener.php
+++ app/Listeners/ContentRestoredListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\ContentRestored;
+use Illuminate\Support\Facades\DB;
 
 class ContentRestoredListener
 {
@@ -16,6 +17,11 @@ class ContentRestoredListener
     {
         if (method_exists($event->model, 'comments')){
             $event->model->comments()->restore();
+            $id = $event->model->comments()->get()->modelKeys();
+            DB::table('attitudes')->where('attitudeable_type', 'App\Models\Comment')
+                                        ->whereIn('attitudeable_id', $id)
+                                        ->whereNotNull ('deleted_at')
+                                        ->update(['deleted_at' => null]);
         }
         if (method_exists($event->model, 'attitude')){
             $event->model->attitude()->restore();
