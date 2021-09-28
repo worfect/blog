d98acae46067bfe5698dc91fc88e2bb3ef2471b4
worfect <senchenkov.93@gmail.com>
2021-09-29 03:58:44 +0800

    -

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() and Auth::user()->isAdministrator() == 1) {
+            return $next($request);
+        }
+        abort(403);
+    }
+}
