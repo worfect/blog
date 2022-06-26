6af0d27a0d22fbe569b087cc504797daf9edd363
worfect <senchenkov.93@gmail.com>
2022-06-26 14:49:54 +0800

    upd front gallery menu

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -7,11 +7,23 @@
 
 @section('content')
     @parent
-    <div class="menu-gallery">
-        @include('gallery.menu')
-    </div>
-    <div class="content-gallery">
-        @include('gallery.content')
+    <div class="gallery">
+        <div class="btn-gallery-menu-collapse d-lg-none row" type="button" data-bs-toggle="collapse" data-bs-target="#collapseGalleryMenu" aria-expanded="false" aria-controls="collapseHeader" id="collapseGalleryMenuBtn">
+            <button class="gallery-menu-down">
+                Gallery menu<i class="fa-solid fa-caret-down"></i>
+            </button>
+            <button class="gallery-menu-up">
+                Gallery menu<i class="fa-solid fa-caret-up "></i>
+            </button>
+        </div>
+        <div class="collapse d-lg-flex gallery-menu-collapse row" id="collapseGalleryMenu">
+            <div class="menu-gallery row">
+                @include('gallery.menu')
+            </div>
+        </div>
+        <div class="content-gallery">
+            @include('gallery.content')
+        </div>
     </div>
 @endsection
 
--- resources/views/gallery/menu.blade.php
+++ resources/views/gallery/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="search-section">
+<div class="search-section col-auto">
     <form class ="quick-search-form" method="GET" action="{{ route('search') }}">
         <input class="search-query form-control" type="text" placeholder="Gallery quick search" name="search-query" >
         <input class="section" type="hidden" name="section" value="gallery">
@@ -7,36 +7,32 @@
     </form>
 </div>
 
-
-<div class="filter col-9">
-    <form class="form-inline" name="content-filter" method="GET" action="{{ route('gallery.index') }}">
+<div class="filter col-auto">
+    <form class="filter-form row align-items-center" name="content-filter" method="GET" action="{{ route('gallery.index') }}">
         <input type="hidden" name="section" value="content">
-        <div class="selector-group">
-            <select class="custom-select" name="method" id="content-method">
-                <option @if(request('method') == 'new') selected @endif value="new">New</option>
-                <option @if(request('method') == 'old') selected @endif value="old">Old</option>
-                <option @if(request('method') == 'best') selected @endif value="best">Best</option>
-            </select>
-            <select class="custom-select" name="criterion" id="content-criterion" disabled>
-                <option @if(request('criterion') == 'views') selected @endif value="views">By Views</option>
-                <option @if(request('criterion') == 'rating') selected @endif value="rating">By Rating</option>
-                <option @if(request('criterion') == 'comments') selected @endif value="comments">By Comments</option>
-            </select>
-            <select class="custom-select" name="date" id="content-date" disabled>
-                <option @if(request('date') == 'all') selected @endif value="all">All</option>
-                <option @if(request('date') == '1') selected @endif value="1">1 Day</option>
-                <option @if(request('date') == '3') selected @endif value="3">3 Days</option>
-                <option @if(request('date') == '7') selected @endif value="7">7 Days</option>
-                <option @if(request('date') == '30') selected @endif value="30">30 Days</option>
-                <option @if(request('date') == 'year') selected @endif value="year">Year</option>
-            </select>
-        </div>
-        <button type="submit" class="btn">Apply</button>
+        <select class="form-select col" name="method" id="content-method">
+            <option @if(request('method') == 'new') selected @endif value="new">New</option>
+            <option @if(request('method') == 'old') selected @endif value="old">Old</option>
+            <option @if(request('method') == 'best') selected @endif value="best">Best</option>
+        </select>
+        <select class="form-select col" name="criterion" id="content-criterion" disabled>
+            <option @if(request('criterion') == 'views') selected @endif value="views">By Views</option>
+            <option @if(request('criterion') == 'rating') selected @endif value="rating">By Rating</option>
+            <option @if(request('criterion') == 'comments') selected @endif value="comments">By Comments</option>
+        </select>
+        <select class="form-select col" name="date" id="content-date" disabled>
+            <option @if(request('date') == 'all') selected @endif value="all">All</option>
+            <option @if(request('date') == '1') selected @endif value="1">1 Day</option>
+            <option @if(request('date') == '3') selected @endif value="3">3 Days</option>
+            <option @if(request('date') == '7') selected @endif value="7">7 Days</option>
+            <option @if(request('date') == '30') selected @endif value="30">30 Days</option>
+            <option @if(request('date') == 'year') selected @endif value="year">Year</option>
+        </select>
+        <button type="submit" class="btn col">Apply</button>
     </form>
 </div>
 
-
-<div class="gallery-user-menu col-5">
+<div class="gallery-user-menu col-auto">
     @if(Auth::user())
         <div class="user">
             <button type="button" class="btn create-gallery-item" data-toggle="modal">
